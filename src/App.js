52570d1d5509cf43575f6d675f61e62112c456af
Manoj Patel BM <manojpatelbm182005@gmail.com>
2025-10-18 08:55:20 +0530

    Updated my work for today

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-
-function App() {
-  return (
-    <h1> Functional Component</h1>
-  );
+import React from "react";
+export default class App extends React.Component{
+  render(){
+    return <h1>Class Component</h1>
+  }
 }
-
-export default App;
